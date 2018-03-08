91d7c99052773636db0a56f214d1f879335387a0
devpurohit <rahulatff@gmail.com>
2018-03-08 08:54:12 +0530

    Class Made

--- sflo.py
+++ sflo.py
@@ -4,73 +4,67 @@ import pandas as pd
 import ipaddress
 import matplotlib.pyplot as plt
 
-# The header of the UFW log files
-names = ["Timestamp", "Customer ID", "Host", "Log file", "Log sequence no.", "Entry type", "Entry identifier",
+
+class log:
+    names = ["Timestamp", "Customer ID", "Host", "Log file", "Log sequence no.", "Entry type", "Entry identifier",
          "User,if", "Reporting IP/host", "Source IP,if", "Source port,if", "Destination IP, if", "Destination Port, if",
          "Text field1", "Text field2", "Text field3", "Numeric field1", "Numeric field2"]
+    path = r"C:\Users\Rahul\Desktop\Logs"
 
-# Raw path of the ufw logs
-path = r'C:\Users\Rahul\Desktop\Logs'
-all_logs = glob.glob(os.path.join(path, "*.csv"))
+    def __init__(self):
+        self.all_logs = glob.glob(os.path.join(log.path, "*.csv"))
+        self.concatlog()
 
-# Show all the found log files
-# for i in all_logs:
-#    print(i, "\n", end="")
+    def concatlog(self):
+        self.dfa = []
+        for f in self.all_logs:
+            self.df = pd.read_csv(f, header=None)
+            self.dfa.append(self.df)
+        self.result = pd.concat(self.dfa)
+        self.result.to_csv('res.csv')    
 
-# Concatenating all log files into one
-dfa = []
-for f in all_logs:
-    df = pd.read_csv(f, header=None)
-    dfa.append(df)
-result = pd.concat(dfa)
-result.to_csv('res.csv')
-
-# Dropping unwanted columns
-result.drop(result.columns[[0, 1, 2, 3, 4, 5, 6, 7, 8, 14, 15, 16, 17]], axis=1, inplace=True)
-result = result.T.reset_index(drop=True).T
-
-# Converting IP Address to int form
-result[0] = result[0].map(lambda x: (ipaddress.IPv4Address(x)))
-# Another DF with only unique pings
-unik = result.drop_duplicates(subset=0)
-# Melting data to find count of IPs
-ip_melt = pd.value_counts(result[0].values, sort=True)
-# Melted data to find count of ports Ports
-port_melt = pd.value_counts(result[3].values, sort=True)
-
-
-# Function to print the total number of IP encountered.
-def count_unik():
-    print(len(unik.index), "IPs")
+    def dataready(self):
+        self.result.drop(self.result.columns[[0, 1, 2, 3, 4, 5, 6, 7, 8, 14, 15, 16, 17]], axis=1, inplace=True)    
+        self.result = self.result.T.reset_index(drop=True).T
+        # Converting IP Address to int form
+        self.result[0] = self.result[0].map(lambda x: (ipaddress.IPv4Address(x)))
+        # Another DF with only unique pings
+        self.unik = self.result.drop_duplicates(subset=0)
+        # Melting data to find count of IPs
+        self.ip_melt = pd.value_counts(self.result[0].values, sort=True)
+        # Melted data to find count of ports Ports
+        self.port_melt = pd.value_counts(self.result[3].values, sort=True)
+    
+    # Function to print the total number of IP encountered.
+    def count_unik(self):
+        print(len(self.unik.index), "IPs")
     
     
-# Function to print 'n' first IPs
-def unik_ip():
-    n = int(input("Enter the 'N'\n"))
-    print("The top", n, "IPs are: ")
-    print(ip_melt[0:n])
+    # Function to print 'n' first IPs
+    def unik_ip(self):
+        n = int(input("Enter the 'N'\n"))
+        print("The top", n, "IPs are: ")
+        print(self.ip_melt[0:n])
 
 
-# Function to plot top IPs
-def ip_plt():
-    n = int(input("Enter the 'N':\n"))
-    pplt = ip_melt[:n]
-    pplt.plot(kind='bar')
-    plt.show()
+    # Function to plot top IPs
+    def ip_plt(self):
+        n = int(input("Enter the 'N':\n"))
+        pplt = self.ip_melt[:n]
+        pplt.plot(kind='bar')
+        plt.show()
     
-    
-# Function to print the top n Ports
-def unik_ports():
-    n = int(input("Enter the 'N':\n"))
-    print(port_melt[0:n])
+   # Function to print the top n Ports
+    def unik_ports():
+        n = int(input("Enter the 'N':\n"))
+        print(self.port_melt[0:n])
 
-
-# Function to print a plot of ports
-def port_plt():
-    n = int(input("Enter the 'N':\n"))
-    pplt = port_melt[:n]
-    pplt.plot(kind='bar')
-    plt.show()
+    # Function to print a plot of ports
+    def port_plt():
+        n = int(input("Enter the 'N':\n"))
+        pplt = self.port_melt[:n]
+        pplt.plot(kind='bar')
+        plt.show()
 
 
 # Menu
@@ -81,14 +75,18 @@ print("2. Top 'N' no. of IPs.")
 print("3. Plot Top 'N' IPs")
 print("4. Top 'N' no. of Ports being used.")
 print("5. Plot Top 'N' ports.")
+
+logx = log()
+logx.dataready()
+
 mn = int(input())
 if mn == 1:
-    count_unik()
+    logx.count_unik()
 elif mn == 2:
-    unik_ip()
+    logx.unik_ip()
 elif mn == 3:
-    ip_plt()
+    logx.ip_plt()
 elif mn == 4:
-    unik_ports()
+    logx.unik_ports()
 elif mn == 5:
-    port_plt()
+    logx.port_plt()
