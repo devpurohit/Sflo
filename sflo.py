8f22df76bfb15171bc2b8dac5f46e73414015485
devpurohit <rahulatff@gmail.com>
2018-03-10 17:26:41 +0530

    Less Redundant Data

--- sflo.py
+++ sflo.py
@@ -6,43 +6,56 @@ import matplotlib.pyplot as plt
 
 
 class log:
-    names = ["Timestamp", "Customer ID", "Host", "Log file", "Log sequence no.", "Entry type", "Entry identifier",
-         "User,if", "Reporting IP/host", "Source IP,if", "Source port,if", "Destination IP, if", "Destination Port, if",
-         "Text field1", "Text field2", "Text field3", "Numeric field1", "Numeric field2"]
+    names = ["Timestamp", "SourceIP", "SourcePort", "DPort",
+         "Status"]
     path = r"C:\Users\Rahul\Desktop\Logs"
 
     def __init__(self):
         self.all_logs = glob.glob(os.path.join(log.path, "*.csv"))
-        self.concatlog()
+        self.loginit()
 
-    def concatlog(self):
+    def loginit(self):
         self.dfa = []
         for f in self.all_logs:
             self.df = pd.read_csv(f, header=None)
             self.dfa.append(self.df)
         self.result = pd.concat(self.dfa)
-        self.result.columns = log.names
-        self.result.to_csv('res.csv')    
-
-    def dataready(self):
-        self.result.drop(self.result.columns[[0, 1, 2, 3, 4, 5, 6, 7, 8, 14, 15, 16, 17]], axis=1, inplace=True)    
+        self.result.reset_index(inplace=True, drop=True)
+        self.result.drop(self.result.columns[[1, 2, 3, 4, 5, 6, 7, 8, 11, 14, 15, 16, 17]], axis=1, inplace=True)    
         self.result = self.result.T.reset_index(drop=True).T
+
         # Converting IP Address to int form
-        self.result[0] = self.result[0].map(lambda x: (ipaddress.IPv4Address(x)))
-        # Another DF with only unique pings
-        self.unik = self.result.drop_duplicates(subset=0)
-        # Melting data to find count of IPs
-        self.ip_melt = pd.value_counts(self.result[0].values, sort=True)
-        # Melted data to find count of ports Ports
-        self.port_melt = pd.value_counts(self.result[3].values, sort=True)
+        self.result[1] = self.result[1].map(lambda x: (ipaddress.IPv4Address(x)))      
+        
+        #Adding Column Names
+        self.result.columns = log.names  
+
+        # Clear Redundant Data    
+        self.datacls()
+
+
+    def datacls(self):
+        self.clrdata = self.result.loc[(self.result.SourceIP == self.result.SourceIP.shift(-1)) &
+                  (self.result.SourcePort == self.result.SourcePort.shift(-1)) ] 
+        self.result = self.result[~self.result.index.isin(self.clrdata.index)]
+        self.result.reset_index(inplace=True, drop=True)  
+        print(self.result.head(10))
+        
+        #Saving to an external file
+        self.result.to_csv('res.csv') 
+
     
     # Function to print the total number of IP encountered.
     def count_unik(self):
+        # Another DF with only unique pings
+        self.unik = self.result.drop_duplicates(subset=0)
         print(len(self.unik.index), "IPs")
-    
+
     
     # Function to print 'n' first IPs
     def unik_ip(self):
+        # Melting data to find count of IPs
+        self.ip_melt = pd.value_counts(self.result[0].values, sort=True)
         n = int(input("Enter the 'N'\n"))
         print("The top", n, "IPs are: ")
         print(self.ip_melt[0:n])
@@ -56,12 +69,14 @@ class log:
         plt.show()
     
    # Function to print the top n Ports
-    def unik_ports():
+    def unik_ports(self):
+        # Melted data to find count of ports Ports
+        self.port_melt = pd.value_counts(self.result[3].values, sort=True)
         n = int(input("Enter the 'N':\n"))
         print(self.port_melt[0:n])
 
     # Function to print a plot of ports
-    def port_plt():
+    def port_plt(self):
         n = int(input("Enter the 'N':\n"))
         pplt = self.port_melt[:n]
         pplt.plot(kind='bar')
@@ -78,7 +93,7 @@ print("4. Top 'N' no. of Ports being used.")
 print("5. Plot Top 'N' ports.")
 
 logx = log()
-logx.dataready()
+
 
 mn = int(input())
 if mn == 1:
@@ -91,3 +106,5 @@ elif mn == 4:
     logx.unik_ports()
 elif mn == 5:
     logx.port_plt()
+elif mn == 6:
+    logx.datacls()    
