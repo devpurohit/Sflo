0a57c6ce7537d68318666ae695040affa9973319
devpurohit <rahulatff@gmail.com>
2018-03-14 08:07:09 +0530

    GEOIP Added

--- sflo.py
+++ sflo.py
@@ -3,7 +3,9 @@ import glob
 import pandas as pd
 import ipaddress
 import matplotlib.pyplot as plt
-import geoip
+import geoip2.database
+import traceback
+import logging
 
 class log:
     names = ["Timestamp", "SourceIP", "SourcePort", "DPort",
@@ -55,12 +57,18 @@ class log:
     
     # Function to print 'n' first IPs
     def unik_ip(self):
-        # Melting data to find count of IPs
+       # Melting data to find count of IPs
         self.ip_melt = pd.value_counts(self.result['SourceIP'].values, sort=True)
         n = int(input("Enter the 'N'\n"))
+        reader = geoip2.database.Reader('GeoLite2-City.mmdb')
         print("The top", n, "IPs are: ")
-        print(self.ip_melt[0:n])
-
+        for ip in self.ip_melt[0:n+1].index:
+            ip = str(ip)
+            if ip == "127.0.0.1":
+              continue
+            response = reader.city(ip)
+            print(ip, end="\t")
+            print(response.country.name,"\t", response.city.name)
 
     # Function to plot top IPs
     def ip_plt(self):
@@ -91,48 +99,54 @@ class log:
     def unik_poip(self):
        n = int(input("Enter the 'N':\n")) 
        self.ip_melt = pd.value_counts(self.result['SourceIP'].values, sort=True)
+      
        for ip in self.ip_melt[0:n].index:
-           print(f"Unique ports for the IP : {ip}")
-           temp = self.result.loc[self.result['SourceIP'] == ip, 'DPort']
-           temp = temp.drop_duplicates()
-           temp.reset_index(inplace=True, drop=True)
-           print(temp.head(10))
+        reader = geoip2.database.Reader('GeoLite2-City.mmdb')
 
-    def ip_geo(self):
-        # Melting data to find count of IPs
-        self.ip_melt = pd.value_counts(self.result['SourceIP'].values, sort=True)
-        n = int(input("Enter the 'N'\n"))
-        gi = geoip.open_database(b"en.csv")
-        print("The top", n, "IPs are: ")
-        for ip in self.ip_melt[0:n].index:
-            ip = str(ip)
-            print(gi.record_by_name(ip))
+        try:
+            response = reader.city(ip)
+            geoc = response.country.name
+            geot = response.city.name
+        except Exception:
+            geoc = ""
+            geot = ""
+        print(f"Unique ports for the IP : {ip} \t {geoc} \t {geot}")
+        temp = self.result.loc[self.result['SourceIP'] == ip, 'DPort']
+        temp = temp.drop_duplicates()
+        temp.reset_index(inplace=True, drop=True)
+        print(temp.head(10))
+
+    
 
 # Menu
-print("###Menu###")
-print("Enter desired command:")
-print("1. No. of unique IPs encountered.")
-print("2. Top 'N' no. of IPs.")
-print("3. Plot Top 'N' IPs")
-print("4. Top 'N' no. of Ports being used.")
-print("5. Plot Top 'N' ports.")
-print("6. Unique Ports of Top IPs.")
-print("7. Print Top IPs with GeoLocation.")
+while(True):
+    print(f"{'-'*10}\n###Menu###\n{'-'*10}")
+    print("Enter desired command:")
+    print("1. No. of unique IPs encountered.")
+    print("2. Top 'N' no. of IPs.")
+    print("3. Plot Top 'N' IPs")
+    print("4. Top 'N' no. of Ports being used.")
+    print("5. Plot Top 'N' ports.")
+    print("6. Unique Ports of Top IPs.")
 
-logx = log()
+    logx = log()
 
-mn = int(input())
-if mn == 1:
-    logx.count_unik()
-elif mn == 2:
-    logx.unik_ip()
-elif mn == 3:
-    logx.ip_plt()
-elif mn == 4:
-    logx.unik_ports()
-elif mn == 5:
-    logx.port_plt()
-elif mn == 6:
-    logx.unik_poip() 
-elif mn == 7:
-    logx.ip_geo() 
+    try :
+        mn = int(input())
+    except ValueError:
+        continue
+    if mn>6:
+        print("\nBiased Input\n")
+        continue    
+    if mn == 1:
+        logx.count_unik()
+    elif mn == 2:
+        logx.unik_ip()
+    elif mn == 3:
+        logx.ip_plt()
+    elif mn == 4:
+        logx.unik_ports()
+    elif mn == 5:
+        logx.port_plt()
+    elif mn == 6:
+        logx.unik_poip() 
